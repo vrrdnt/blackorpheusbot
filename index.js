1dfa463c6c48eb81f40bf0128bfc8785f09b8600
Will Swoveland <will.arctic@gmail.com>
2021-03-30 17:36:03 -0700

    Fresh start, sorta. Got rid of funky methods

--- index.js
+++ index.js
@@ -21,9 +21,10 @@ NOTES:
 class Artist {
     constructor(id) {
         this.id = id;
+        this.song_list = []
     }
 
-    async getAllSongs() {
+    async storeAllSongs() {
         // Use a while loop to add song IDs to an array.
         // The condition of the while loop must be dependent on the value of a variable that
         // is instantiated as something truthy, but is then
@@ -39,11 +40,11 @@ class Artist {
 
         while (search) {
             const response = await fetch(`https://api.genius.com/artists/${this.id}/songs?per_page=50&page=${pagination}`, {
-                    method: "GET",
-                    headers: {
-                        Authorization: `Bearer ${process.env.GENIUS_ACCESS_TOKEN}`,
-                    },
-                })
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${process.env.GENIUS_ACCESS_TOKEN}`,
+                },
+            })
             const data = await response.json()
 
             if (!data["response"]["next_page"]) {
@@ -58,67 +59,27 @@ class Artist {
                 }
             }
         }
-        return song_list;
+        return this.song_list = song_list
     }
 
-    async getRandomLyrics() {
-        await this.getAllSongs()
-            .then(list => {
-                const random = Math.floor(Math.random() * list.length)
-                getSongById(list[random], process.env.GENIUS_ACCESS_TOKEN)
-                    .then(song => {
-                        return song.lyrics
-                    })
-            })
+    logAllSongs() {
+        return console.log(this.song_list);
     }
-
-    async genRandomTweet() {
-       await this.getRandomLyrics()
-            .then(lyrics => {
-                // Split lyrics into an array by line
-                const lyrics_array = lyrics.toString().split("\n")
-
-                // Generate array without lines like [Chorus]
-                const line_set = lyrics_array.filter(item => !item.includes("[") && !item.includes("\n"))
-
-                // Generate a random number between 1 and 3 to determine how far up/down the bot will crawl
-                const random = Math.floor(Math.random() * 3) + 1
-
-                // Generate a new random to decide whether the bot will crawl backwards or forwards
-                // 0 is forwards (down the song), 1 is backwards (up the song)
-                const random_direction = Math.round(Math.random())
-
-                // Generate a new random to determine a random starting position
-                const random_start = Math.floor(Math.random() * line_set.length)
-
-                // Return the string that will get Tweeted
-                let base_array = []
-
-
-                // Add lines to array depending on direction and number of lines
-                switch (random_direction) {
-                    case 0:
-                        for (let i = 0; i < random; i++) {
-                            base_array.push(line_set[random_start+i])
-                        }
-                        break;
-                    case 1:
-                        for (let i = 0; i < random; i++) {
-                            base_array.unshift(line_set[random_start-i])
-                        }
-                }
-
-                // Return the finished array as a multi-line string
-                return base_array.join("\n").toString()
-            })
-    }
-
-
 }
 
 // Define all of the artists the bot will randomly pick
-const Milo = new Artist(3158);
-const RAPFerreira = new Artist(1840820)
+
+// Milo
+let Milo = new Artist(3158);
+Milo.storeAllSongs().then()
+
+// Scallops Hotel
+let ScallopsHotel = new Artist(96862);
+ScallopsHotel.storeAllSongs().then()
+
+// R.A.P. Ferreira
+let RAPFerreira = new Artist(1840820);
+RAPFerreira.storeAllSongs().then()
 
 // Build Twitter API client
 const twitterBot = new Twit({
@@ -127,6 +88,3 @@ const twitterBot = new Twit({
     access_token: process.env.TWITTER_ACCESS_TOKEN,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
-
-Milo.genRandomTweet()
-    .then(s => console.log(s))
