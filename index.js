eed1232de86a425c61c1a112ec2cc84f8e8a754f
vrrdnt <will.arctic@gmail.com>
2021-04-07 16:18:38 -0700

    Really messy, but mostly done :-)

--- index.js
+++ index.js
@@ -5,19 +5,60 @@ const fs = require("fs");
 const { getSongById } = require("genius-lyrics-api");
 
 
-// Create a list of Genius Artist IDs to pick from
-    const artist_ids = [3158, 96862, 1840820]; // Milo, Scallops Hotel, R.A.P. Ferreira
+// Create a list of unique Genius Artist IDs to pick from
+const artist_ids = [3158, 96862, 1840820]; // Milo, Scallops Hotel, R.A.P. Ferreira
 
 // Load the JSON file artist_song_ids.json
-    let song_ids = require("./artist_song_ids.json");
+let song_ids = require("./artist_song_ids.json");
 
 // Select a random Artist ID from the list artist_ids
-    let random_artist = artist_ids[Math.floor(Math.random() * artist_ids.length)];
+let random_artist = artist_ids[Math.floor(Math.random() * artist_ids.length)];
 
 // Get a random song ID from the list of song IDs for the randomly-picked artist
-    let random_song = song_ids[random_artist][Math.floor(Math.random() * song_ids[random_artist].length)]
+let random_song = song_ids[random_artist][Math.floor(Math.random() * song_ids[random_artist].length)]
 
-// Grab the lyrics
-let song_lyrics;
-getSongById(random_song, process.env.GENIUS_ACCESS_TOKEN)
-    .then((song) => song_lyrics = song.lyrics)
+// Get random lyrics and tweet them
+getSongById(random_song, process.env.GENIUS_ACCESS_TOKEN).then((song) => {
+    let bars;
+    console.log(song)
+    if (song.lyrics != null) {
+        let arr = song.lyrics.split("\n");
+        for (let i = arr.length - 1; i >= 0; i--) {
+            if (arr[i].charAt(0) === "[" || arr[i].charAt(0) === "") {
+                arr.splice(i, 1);
+            }
+        }
+        let pos = Math.floor(Math.random() * arr.length-2)
+        if (Math.random() < 0.5) {
+            bars = [
+                arr[pos],
+                arr[pos+1],
+                arr[pos+2]
+            ]
+        } else {
+            bars = [
+                arr[pos],
+                arr[pos+1]
+            ]
+        }
+        bars = bars.map(line => line.toLowerCase())
+        console.log(bars)
+
+        const twitterBot = new Twit({
+            consumer_key: process.env.TWITTER_API_KEY,
+            consumer_secret: process.env.TWITTER_API_KEY_SECRET,
+            access_token: process.env.TWITTER_ACCESS_TOKEN,
+            access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+        });
+
+        twitterBot.post(
+            "statuses/update",
+            { status: bars.join('\n') },
+            function (error, data, response) {
+                if (error) {
+                    console.log(error);
+                }
+            }
+        );
+    }
+})
