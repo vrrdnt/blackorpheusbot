a47e31f8c8ed28f7fb64dc949a86e23d2505ffc3
vrrdnt <will.arctic@gmail.com>
2021-10-06 21:17:04 -0700

    lots of good stuff working now

--- index.js
+++ index.js
@@ -1,135 +1,47 @@
 require("dotenv").config();
-
-const { getSongById } = require("genius-lyrics-api");
-const fetch = require('node-fetch');
-const twitterBot = require("./src/twitter.js");
-const fs = require('fs');
+const ArtistSet = require("./src/classes/ArtistSet.js");
+const Tweet = require("./src/classes/Tweet.js");
 // import { loadRecents, saveRecents } from './src/tracking/recent.js';
 
 const config = require("./config.js");
 const path = require("path");
+const Twit = require("twit");
 
-class Tweet {
-    constructor(contents) {
-        this.contents = contents;
-    }
-
-    sendTweet(contents=this.contents) {
-        twitterBot.post('statuses/update', {
-            status: contents
-        },
-        function(error) {
-            if (!error) return
-            console.log(`There was an error: \n${error}`); 
-        });
-        }
-
-    updateName(song_name=null) {
-        if (!song_name) return
-        twitterBot.post(`account/update_profile`, { "name": song_name }, function (err) {
-            if (err) return console.error(err)
-        })
-    }
-}
-
-class ArtistSet {
-
-    // 3158 Milo
-    // 96862 Scallops Hotel
-    // 1840820 R.A.P. Ferreira
-    // 1544550 Nostrum Grocers
-
-    // Individual songs to block
-    // 6393069, 
-
-    constructor(...rest) {
-        this.artist_ids = [...rest];
-        this.discography = [];
-        this.song_object = {
-            lyrics: []
-        }
-    }
-    
-    async fetchSongs(artists=this.artist_ids) {
-        for (const artist of artists) {
-            let page = 1;
-            let response_collector = [];
-            let response_song_collector = [];
-            let loop = true;
-            while (loop) {
-                const response = await fetch(`https://api.genius.com/artists/${artist}/songs?per_page=50&page=${page}`, {
-                        method: "GET",
-                        headers: {
-                            Authorization: `Bearer ${process.env.GENIUS_ACCESS_TOKEN}`,
-                        },
-                    })
-    
-                    const data = await response.json()
-                    let songs = data["response"]["songs"];
-                    let next_page = data["response"]["next_page"];
-    
-                    response_song_collector.push(JSON.stringify(data, null, 4))
-    
-
-                    for (const song in songs) {
-                        response_collector.push([songs[song]["id"], songs[song]["title"]])
-                    }
-    
-                    if (!next_page) {
-                        loop = false;
-                        return this.discography = response_collector.map(song => (song));
-                    } else {
-                        page = next_page;
-                    }
-                }
-        }
-    }
-
-    async download(id=0) {
-        if (!id) return console.error("No ID supplied.")
-        let try_again = true;
-        while (try_again) {
-            const song_object = await getSongById(id, process.env.GENIUS_ACCESS_TOKEN);
-            if(song_object.lyrics) {
-                try_again = false;
-            }
-        
-            this.song_object = song_object;
-        }
-
-        this.song_object.lyrics = this.song_object.lyrics.split("\n")
-
-        for (let i = this.song_object.lyrics.length - 1; i >= 0; i--) {
-            if (this.song_object.lyrics[i].charAt(0) === '[' || 
-            this.song_object.lyrics[i].charAt(0) === '' ||
-             this.song_object.lyrics[i].charAt(0) === '(') {
-                this.song_object.lyrics.splice(i, 1);
-            }
-        }
-    }
-
-    random() {
-        return this.discography[Math.floor(Math.random() * this.discography.length)];
-    }
-}
-
-const test_artist = new ArtistSet(3158, 96862, 1840820, 1544550);
+const artist_set = new ArtistSet(3158, 96862, 1840820, 1544550);
 
 async function main() {
-    await test_artist.fetchSongs();
+    await artist_set.fetchSongs();
 
-    const random_song = test_artist.random();
+    const random_song = artist_set.randomSong();
     const id = random_song[0];
     const title = random_song[1];
 
-    await test_artist.download(id)
+    const url = await artist_set.download(id)
 
-    const tweetObject = new Tweet("test")
-    console.log(test_artist.song_object.lyrics);
+    const random_set = artist_set.randomSet();
+
+    console.log(random_set);
+
+    console.log(artist_set.song_object.id)
+
+    const tweet = new Tweet(artist_set.song_object.name);
+
+    // tweet.sendTweet(random_set.join("\n"));
+
+    // tweet.updateName(title)
+
+    // await tweet.updateProfileImage(url);
+
 }
 
 main();
 
 // TODO: Pre-generate a tweet and DM it to vrrdnt. Allow a "yes" or "no" response to post the tweet,
-// or block that song ID from entering the available song pool
-// Disallow lines past a certain length (280 for max tweet length, aim to cut off at closest sentence)
+// or block that song ID from entering the available song pool (need persistent storage, no heroku)
+
+// TODO: Set profile picture to the provided album art on Genius. If the default is the result (or some key in the JSON response
+// says something about not having album art) then set it to a default of our choosing.
+
+// TODO: Disallow lines past a certain length (280 for max tweet length, aim to cut off at closest finished sentence)
+
+// TODO: Implement some kind of crawl. No way to clean strings without potentially ruining a tweet.
---
+++ src/classes/ArtistSet.js
@@ -0,0 +1,111 @@
+const { getSongById } = require("genius-lyrics-api");
+const fetch = require('node-fetch');
+const { SSL_OP_EPHEMERAL_RSA } = require("constants");
+const fs = require('fs')
+
+class ArtistSet {
+
+    // 3158 Milo
+    // 96862 Scallops Hotel
+    // 1840820 R.A.P. Ferreira
+    // 1544550 Nostrum Grocers
+
+    // Individual songs to block
+    BLOCKED_SONG_IDS = [
+        6393069, 7105027, 3420106,
+        4051931
+    ]
+
+    constructor(...rest) {
+        this.artist_ids = [...rest];
+        this.discography = [];
+        this.song_object = {
+            lyrics: []
+        }
+    }
+    
+    async fetchSongs(artists=this.artist_ids) {
+        let counter = 0;
+        for (const artist of artists) {
+            let page = 1;
+            let response_collector = [];
+            let response_song_collector = [];
+            let loop = true;
+            while (loop) {
+                const response = await fetch(`https://api.genius.com/artists/${artist}/songs?per_page=50&page=${page}`, {
+                        method: "GET",
+                        headers: {
+                            Authorization: `Bearer ${process.env.GENIUS_ACCESS_TOKEN}`,
+                        },
+                    })
+    
+                    const data = await response.json()
+                    let songs = data["response"]["songs"];
+                    let next_page = data["response"]["next_page"];
+    
+                    response_song_collector.push(JSON.stringify(data, null, 4))
+
+                    songs.forEach(song => {
+                        if (!this.artist_ids.some(artist_id => String(song["primary_artist"]["id"]).includes(artist_id))) return
+                        if (this.BLOCKED_SONG_IDS.some(blocked_id => String(song["id"]).includes(blocked_id))) return 
+                        
+                        response_collector.push([song["id"], song["title"]])
+                    });
+    
+                    if (!next_page) {
+                        loop = false;
+                        return this.discography = response_collector.map(song => (song));
+                    } else {
+                        page = next_page;
+                    }
+                await new Promise(r => setTimeout(r, 20));
+                }
+                counter += 1;
+        }
+    }
+
+    async download(id=0) {
+        if (!id) return console.error("No ID supplied.")
+        let try_again = true;
+        let song_object = { lyrics: [] };
+        while (try_again) {
+            song_object = await getSongById(id, process.env.GENIUS_ACCESS_TOKEN);
+            if(song_object.lyrics) {
+                try_again = false;
+            }
+        
+            this.song_object = song_object
+        }
+
+        this.song_object.lyrics = this.song_object.lyrics.split(/\n/);
+
+        for (let i = this.song_object.lyrics.length - 1; i >= 0; i--) {
+            if (this.song_object.lyrics[i].charAt(0) === '[' ||
+            this.song_object.lyrics[i].charAt(0) === '' ||
+            this.song_object.lyrics[i].includes("?")) { 
+                this.song_object.lyrics.splice(i, 1);
+            }
+        }
+
+        const extension = `${this.song_object.albumArt.slice(this.song_object.albumArt.lastIndexOf(".") + 1, this.song_object.albumArt.length)}`
+        fs.writeFileSync(`image.${extension}`, await (await fetch(this.song_object.albumArt)).buffer(), { encoding: "base64" })
+
+        return extension;
+    }
+
+    randomSong() {
+        return this.discography[Math.floor(Math.random() * this.discography.length)];
+    }
+
+    randomSet() {
+        const batch = [];
+
+        const random = Math.floor(Math.random() * this.song_object.lyrics.length-2)
+        
+        batch.push(this.song_object.lyrics[random],this.song_object.lyrics[random + 1], this.song_object.lyrics[random + 2])
+        
+        return batch
+    }
+}
+
+module.exports = ArtistSet;
