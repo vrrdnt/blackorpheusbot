d406f16dd5b3b9a372a429cce988b68e85c67650
vrrdnt <will.arctic@gmail.com>
2022-12-04 01:06:36 -0800

    lol gotta regenerate keys. oops

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    twitter_api_key: 'aEPx4m2NLHuJwC1cdrQWdjtNj',
-    twitter_api_key_secret: '9T8ItZ2za8vczMV9m7tMH9BX7IwtUQf393AsEYN5uCxRqy77Gd',
-    twitter_access_token: '1370151080986775552-3epuonkJIA5N65G5ZcCEaLxHLhHg00',
-    twitter_access_token_secret: 'FoBts9WgrvycLxOtKf395cUDHhYlgrluJgomYe3IkkdxW',
-    genius_access_token: 'V2UodQfdYuNEuCxsxX-lE08uzzJR3eTtfM7Xq-ox37TIOnrylbvcEaPu2m_GL4xN',
-    artists: ['3158', '96862', '1840820'], // list of strs or str
+    twitter_api_key: '',
+    twitter_api_key_secret: '',
+    twitter_access_token: '',
+    twitter_access_token_secret: '',
+    genius_access_token: '',
+    artists: [''], // list of strs or str
     tweet_schedule: '0 10-16/2 * * *', // every at minute 0 past every 2nd hour from 10 through 16
     cache_interval: '0 0 * * *', //every day at midnight
     avoid_repeats: true, // compare against recent tweets, restart process if selected bars are contained in recent tweet
